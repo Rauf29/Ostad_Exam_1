88992167469828e9f95e76031167f9e27059f403
Rauf29 <rauf.official2020@gmail.com>
2024-10-18 18:11:03 +0600

    Correction: validation with switch case.

--- student_result/result.php
+++ student_result/result.php
@@ -2,48 +2,53 @@
 
 function studentResult( ...$subjects ) {
     foreach ( $subjects as $subject ) {
-        if ( $subject < 0 || $subject > 100 || count( $subjects ) != 5 ) {
-            return "Mark range is invalid";
-        } elseif ( $subject < 33 ) {
-            return "Result: Fail";
-        } else {
-            $totalNumber = array_sum( $subjects );
-            $totalSubject = count( $subjects );
-            $averageNumber = $totalNumber / $totalSubject;
-            switch ( $averageNumber ) {
-            case ( $averageNumber >= 80 ):
-                $result = "Grade: A+";
-                break;
-
-            case ( $averageNumber >= 70 ):
-                $result = "Grade: A";
-                break;
-
-            case ( $averageNumber >= 60 ):
-                $result = "Grade: A-";
-                break;
-            case ( $averageNumber >= 50 ):
-                $result = "Grade: B";
-                break;
-
-            case ( $averageNumber >= 40 ):
-                $result = "Grade: C";
-                break;
-            case ( $averageNumber >= 33 ):
-                $result = "Grade: D";
-                break;
-            default:
-                $result = "Grade: F";
-            }
+        switch ( $subject ) {
+        case ( $subject < 0 || $subject > 100 || count( $subjects ) != 5 ):
+            echo "Invalid mark range";
+            return;
+        }
+        switch ( $subject ) {
+        case ( $subject < 33 ):
+            echo "You have failed";
+            return;
         }
     }
-    return "Total Marks: {$totalNumber} \nAverage Marks: {$averageNumber} \nGrade : {$result} ";
+    $totalNumber = array_sum( $subjects );
+    $totalSubject = count( $subjects );
+    $averageNumber = $totalNumber / $totalSubject;
+    switch ( $averageNumber ) {
+    case ( $averageNumber >= 80 ):
+        $result = "A+";
+        break;
 
+    case ( $averageNumber >= 70 ):
+        $result = "A";
+        break;
+
+    case ( $averageNumber >= 60 ):
+        $result = "A-";
+        break;
+    case ( $averageNumber >= 50 ):
+        $result = "B";
+        break;
+
+    case ( $averageNumber >= 40 ):
+        $result = "C";
+        break;
+    case ( $averageNumber >= 33 ):
+        $result = "D";
+        break;
+    default:
+        $result = "F";
+    }
+    echo "Total Number: $totalNumber \n";
+    echo "Average Number: $averageNumber \n";
+    echo "Grade: $result";
 }
 
-echo studentResult( 54, 34, 54, 54, 86 ) . "\n"; // 5 subject all pass all subjects
-echo studentResult( 54, 34, 54, 54, 86, 32 ) . "\n"; // if try to input more then 5 subjects
-echo studentResult( 54, 34, 54, 54, ) . "\n"; // if try to input less then 5 subjects
-echo studentResult( 54, 34, 54, -4, 86 ) . "\n"; // if input any subject mark less then 0
-echo studentResult( 54, 34, 54, 54, 102 ) . "\n"; // if input any subject mark more then 100
-echo studentResult( 54, 34, 54, 3, 86 ) . "\n"; // if any subject mark less then 33
+studentResult( 54, 34, 54, 54, 86 ) . "\n"; // 5 subject all pass all subjects
+//studentResult( 54, 34, 54, 54, 86, 32 ) . "\n"; // if try to input more then 5 subjects
+//studentResult( 54, 34, 54, 54, ) . "\n"; // if try to input less then 5 subjects
+//studentResult( 54, 34, 54, -4, 86 ) . "\n"; // if input any subject mark less then 0
+//studentResult( 54, 34, 54, 54, 102 ) . "\n"; // if input any subject mark more then 100
+//studentResult( 54, 34, 54, 3, 86 ) . "\n"; // if any subject mark less then 33
